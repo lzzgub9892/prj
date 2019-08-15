b49f7c401f08586a8f151aa3eb24eebb966c1099
lzz <695180353@qq.com>
2019-08-15 15:18:07 +0800

    搭建项目环境

---
+++ src/main/java/com/woniu/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.woniu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class LoginController {
+
+	@RequestMapping("save")
+	public String save() {
+		System.out.println("LoginController.save()");
+		return null;
+	}
+}
